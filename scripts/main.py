db25ff4199e31925468222dae2b00a912e224f64
Ubuntu <ubuntu@ip-172-31-40-235.eu-central-1.compute.internal>
2020-03-04 14:25:49 +0000

    changes

--- scripts/main.py
+++ scripts/main.py
@@ -41,16 +41,16 @@ import tensorflow as tf
 GPU_FRACTION = 1
 
 print ('Tensorflow version ', str(tf.__version__))
-if tf.__version__ < "1.14.0":
-	config = tf.ConfigProto()
-	config.gpu_options.per_process_gpu_memory_fraction = GPU_FRACTION
+#if tf.__version__ < "1.14.0":#
+#	config = tf.ConfigProto()
+#	config.gpu_options.per_process_gpu_memory_fraction = GPU_FRACTION
 	#config.gpu_options.allow_growth = True
-	session = tf.Session(config=config)
-else:
-	config = tf.compat.v1.ConfigProto()
-	config.gpu_options.per_process_gpu_memory_fraction = GPU_FRACTION
+#	session = tf.Session(config=config)
+#else:
+#	config = tf.compat.v1.ConfigProto()
+#	config.gpu_options.per_process_gpu_memory_fraction = GPU_FRACTION
 	#config.gpu_options.allow_growth = True
-	session = tf.compat.v1.Session(config=config)
+#	session = tf.compat.v1.Session(config=config)
 
 
 class GoalBabbling():
